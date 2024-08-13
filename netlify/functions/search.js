7460df2d7b8b1c0ba6408bae29a322b7eb17dcbe
김 한솔 <seobro@gim-ui-MacBookAir.local>
2024-08-13 16:22:36 +0900

    Increase results count, add scroll functionality, and fix N/A issues

--- netlify/functions/search.js
+++ netlify/functions/search.js
@@ -4,10 +4,9 @@ exports.handler = async function(event, context) {
   const query = event.queryStringParameters.q;
 
   try {
-    const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=${process.env.YOUTUBE_API_KEY}`);
+    const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${query}&key=${process.env.YOUTUBE_API_KEY}`);
     const data = await response.json();
 
-    // 데이터가 올바르게 반환되는지 로그로 확인
     console.log('Fetched Data:', data);
 
     return {
